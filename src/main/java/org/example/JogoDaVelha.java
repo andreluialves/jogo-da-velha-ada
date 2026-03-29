24219338c62ca7b54ca3232fdf25f7291feafeb4
André Alves <andreluialves@hotmail.com>
2026-03-29 19:10:02 -0300

    feat: creates reading of the play

--- src/main/java/org/example/JogoDaVelha.java
+++ src/main/java/org/example/JogoDaVelha.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class JogoDaVelha {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         char[][] tabuleiro = new char[3][3];
         inicializarTabuleiro(tabuleiro);
         char jogadorAtual = 'X';
+
+        System.out.print("Linha (0-2): ");
+        int linha = sc.nextInt();
+
+        System.out.print("Coluna (0-2): ");
+        int coluna = sc.nextInt();
     }
 
     public static void inicializarTabuleiro(char[][] tabuleiro) {
