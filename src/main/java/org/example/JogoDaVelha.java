0e2786b39860875bbce88fa2421c772af5025a86
André Alves <andreluialves@hotmail.com>
2026-03-28 23:40:41 -0300

    feat: displays the board

--- src/main/java/org/example/JogoDaVelha.java
+++ src/main/java/org/example/JogoDaVelha.java
@@ -1,22 +1,26 @@
 package org.example;
 
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class JogoDaVelha {
     public static void main(String[] args) {
         char[][] tabuleiro = new char[3][3];
-        public static void inicializarTabuleiro(char[][] tabuleiro) {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    tabuleiro[i][j] = ' ';
-                }
+        inicializarTabuleiro(tabuleiro);
+    }
+
+    public static void inicializarTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tabuleiro[i][j] = ' ';
             }
-
         }
+    }
 
-
+    public static void mostrarTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(tabuleiro[i][0] + " | " + tabuleiro[i][1] + " | " + tabuleiro[i][2]);
+            if (i < 2) {
+                System.out.println("--+---+--");
+            }
+        }
     }
 }
 
